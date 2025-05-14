3b16009d2dfdf62946883772644da8d5da95e2c2
James B <js-james@outlook.com>
2025-05-14 19:49:27 +0100

    Using this branch to build the program for now

--- jsb-binview/frmMain.Designer.cs
+++ jsb-binview/frmMain.Designer.cs
@@ -29,19 +29,72 @@
 		private void InitializeComponent()
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+			menuStrip1 = new MenuStrip();
+			fileToolStripMenuItem = new ToolStripMenuItem();
+			dgvMain = new DataGridView();
+			btnOpen = new Button();
+			menuStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)dgvMain).BeginInit();
 			SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+			menuStrip1.Location = new Point(0, 0);
+			menuStrip1.Name = "menuStrip1";
+			menuStrip1.Size = new Size(800, 24);
+			menuStrip1.TabIndex = 0;
+			menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			fileToolStripMenuItem.Size = new Size(37, 20);
+			fileToolStripMenuItem.Text = "File";
+			// 
+			// dgvMain
+			// 
+			dgvMain.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			dgvMain.Location = new Point(91, 239);
+			dgvMain.Name = "dgvMain";
+			dgvMain.Size = new Size(240, 150);
+			dgvMain.TabIndex = 1;
+			// 
+			// btnOpen
+			// 
+			btnOpen.Location = new Point(540, 161);
+			btnOpen.Name = "btnOpen";
+			btnOpen.Size = new Size(117, 57);
+			btnOpen.TabIndex = 2;
+			btnOpen.Text = "Open Binary";
+			btnOpen.UseVisualStyleBackColor = true;
+			btnOpen.Click += btnOpen_Click;
 			// 
 			// frmMain
 			// 
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
 			ClientSize = new Size(800, 450);
+			Controls.Add(btnOpen);
+			Controls.Add(dgvMain);
+			Controls.Add(menuStrip1);
 			Icon = (Icon)resources.GetObject("$this.Icon");
+			MainMenuStrip = menuStrip1;
 			Name = "frmMain";
 			Text = "BinView";
+			Load += frmMain_Load;
+			menuStrip1.ResumeLayout(false);
+			menuStrip1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)dgvMain).EndInit();
 			ResumeLayout(false);
+			PerformLayout();
 		}
 
 		#endregion
+
+		private MenuStrip menuStrip1;
+		private ToolStripMenuItem fileToolStripMenuItem;
+		private DataGridView dgvMain;
+		private Button btnOpen;
 	}
 }
--- jsb-binview/frmMain.cs
+++ jsb-binview/frmMain.cs
@@ -1,10 +1,43 @@
 namespace jsb_binview
 {
-    public partial class frmMain : Form
-    {
-        public frmMain()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class frmMain : Form
+	{
+		//Variables
+		string binFileLocation = "D:\\Data\\binit\\binit.bin";
+
+		public frmMain()
+		{
+			InitializeComponent();
+		}
+
+		private void frmMain_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void OpenBinaryFile() 
+		{
+
+
+			using (BinaryReader breader = new BinaryReader(new FileStream(binFileLocation, FileMode.Open))) 
+			{
+
+			}
+
+
+
+
+				DisplayDataChunkList(01);
+		}
+		
+		private void DisplayDataChunkList(int bVersion)
+		{
+
+		}
+
+		private void btnOpen_Click(object sender, EventArgs e)
+		{
+			OpenBinaryFile();
+		}
+	}
 }
