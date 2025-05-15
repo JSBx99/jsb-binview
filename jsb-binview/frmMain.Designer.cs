5c11332196dc2f981461ef79b01a5240d9de3a7c
James <js-james@outlook.com>
2025-05-16 00:16:06 +0100

    Set up basic UI. Program isn't very functional at the moment and still needs core features implementing

--- jsb-binview/frmMain.Designer.cs
+++ jsb-binview/frmMain.Designer.cs
@@ -33,8 +33,19 @@
 			fileToolStripMenuItem = new ToolStripMenuItem();
 			dgvMain = new DataGridView();
 			btnOpen = new Button();
+			tableLayoutPanel1 = new TableLayoutPanel();
+			label1 = new Label();
+			splitContainer1 = new SplitContainer();
+			txtBinaryVersion = new TextBox();
+			lblBinaryVersion = new Label();
+			txtDataChunkCount = new TextBox();
 			menuStrip1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)dgvMain).BeginInit();
+			tableLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+			splitContainer1.Panel1.SuspendLayout();
+			splitContainer1.Panel2.SuspendLayout();
+			splitContainer1.SuspendLayout();
 			SuspendLayout();
 			// 
 			// menuStrip1
@@ -55,28 +66,110 @@
 			// dgvMain
 			// 
 			dgvMain.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			dgvMain.Location = new Point(91, 239);
+			dgvMain.Dock = DockStyle.Fill;
+			dgvMain.Location = new Point(0, 0);
 			dgvMain.Name = "dgvMain";
-			dgvMain.Size = new Size(240, 150);
+			dgvMain.Size = new Size(595, 391);
 			dgvMain.TabIndex = 1;
 			// 
 			// btnOpen
 			// 
-			btnOpen.Location = new Point(540, 161);
+			btnOpen.Dock = DockStyle.Bottom;
+			btnOpen.Location = new Point(0, 334);
 			btnOpen.Name = "btnOpen";
-			btnOpen.Size = new Size(117, 57);
+			btnOpen.Size = new Size(187, 57);
 			btnOpen.TabIndex = 2;
 			btnOpen.Text = "Open Binary";
 			btnOpen.UseVisualStyleBackColor = true;
 			btnOpen.Click += btnOpen_Click;
+			// 
+			// tableLayoutPanel1
+			// 
+			tableLayoutPanel1.ColumnCount = 6;
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 90F));
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 100F));
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 115F));
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 100F));
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+			tableLayoutPanel1.Controls.Add(label1, 2, 0);
+			tableLayoutPanel1.Controls.Add(splitContainer1, 0, 1);
+			tableLayoutPanel1.Controls.Add(txtBinaryVersion, 1, 0);
+			tableLayoutPanel1.Controls.Add(lblBinaryVersion, 0, 0);
+			tableLayoutPanel1.Controls.Add(txtDataChunkCount, 3, 0);
+			tableLayoutPanel1.Dock = DockStyle.Fill;
+			tableLayoutPanel1.Location = new Point(0, 24);
+			tableLayoutPanel1.Name = "tableLayoutPanel1";
+			tableLayoutPanel1.RowCount = 2;
+			tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 25F));
+			tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+			tableLayoutPanel1.Size = new Size(800, 426);
+			tableLayoutPanel1.TabIndex = 3;
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Dock = DockStyle.Left;
+			label1.Location = new Point(193, 0);
+			label1.Name = "label1";
+			label1.Size = new Size(108, 25);
+			label1.TabIndex = 7;
+			label1.Text = "Data Chunk Count:";
+			label1.TextAlign = ContentAlignment.MiddleLeft;
+			// 
+			// splitContainer1
+			// 
+			splitContainer1.BorderStyle = BorderStyle.Fixed3D;
+			tableLayoutPanel1.SetColumnSpan(splitContainer1, 6);
+			splitContainer1.Dock = DockStyle.Fill;
+			splitContainer1.Location = new Point(3, 28);
+			splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			splitContainer1.Panel1.Controls.Add(dgvMain);
+			// 
+			// splitContainer1.Panel2
+			// 
+			splitContainer1.Panel2.Controls.Add(btnOpen);
+			splitContainer1.Panel2.Paint += splitContainer1_Panel2_Paint;
+			splitContainer1.Size = new Size(794, 395);
+			splitContainer1.SplitterDistance = 599;
+			splitContainer1.TabIndex = 4;
+			// 
+			// txtBinaryVersion
+			// 
+			txtBinaryVersion.Location = new Point(93, 3);
+			txtBinaryVersion.Name = "txtBinaryVersion";
+			txtBinaryVersion.ReadOnly = true;
+			txtBinaryVersion.Size = new Size(94, 23);
+			txtBinaryVersion.TabIndex = 5;
+			// 
+			// lblBinaryVersion
+			// 
+			lblBinaryVersion.AutoSize = true;
+			lblBinaryVersion.Dock = DockStyle.Left;
+			lblBinaryVersion.Location = new Point(3, 0);
+			lblBinaryVersion.Name = "lblBinaryVersion";
+			lblBinaryVersion.Size = new Size(84, 25);
+			lblBinaryVersion.TabIndex = 6;
+			lblBinaryVersion.Text = "Binary Version:";
+			lblBinaryVersion.TextAlign = ContentAlignment.MiddleLeft;
+			// 
+			// txtDataChunkCount
+			// 
+			txtDataChunkCount.Location = new Point(308, 3);
+			txtDataChunkCount.Name = "txtDataChunkCount";
+			txtDataChunkCount.ReadOnly = true;
+			txtDataChunkCount.Size = new Size(94, 23);
+			txtDataChunkCount.TabIndex = 8;
 			// 
 			// frmMain
 			// 
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
 			ClientSize = new Size(800, 450);
-			Controls.Add(btnOpen);
-			Controls.Add(dgvMain);
+			Controls.Add(tableLayoutPanel1);
 			Controls.Add(menuStrip1);
 			Icon = (Icon)resources.GetObject("$this.Icon");
 			MainMenuStrip = menuStrip1;
@@ -86,6 +179,12 @@
 			menuStrip1.ResumeLayout(false);
 			menuStrip1.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)dgvMain).EndInit();
+			tableLayoutPanel1.ResumeLayout(false);
+			tableLayoutPanel1.PerformLayout();
+			splitContainer1.Panel1.ResumeLayout(false);
+			splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+			splitContainer1.ResumeLayout(false);
 			ResumeLayout(false);
 			PerformLayout();
 		}
@@ -96,5 +195,11 @@
 		private ToolStripMenuItem fileToolStripMenuItem;
 		private DataGridView dgvMain;
 		private Button btnOpen;
+		private TableLayoutPanel tableLayoutPanel1;
+		private SplitContainer splitContainer1;
+		private TextBox txtBinaryVersion;
+		private Label lblBinaryVersion;
+		private Label label1;
+		private TextBox txtDataChunkCount;
 	}
 }
--- jsb-binview/frmMain.cs
+++ jsb-binview/frmMain.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace jsb_binview
 {
 	public partial class frmMain : Form
@@ -15,29 +17,90 @@ namespace jsb_binview
 
 		}
 
-		private void OpenBinaryFile() 
+		private void OpenBinaryFile()
 		{
 
 
-			using (BinaryReader breader = new BinaryReader(new FileStream(binFileLocation, FileMode.Open))) 
+			using (BinaryReader breader = new BinaryReader(new FileStream(binFileLocation, FileMode.Open)))
 			{
+				byte[] bHead = breader.ReadBytes(4); //Get header bytes
+				byte[] bFileStructureType = breader.ReadBytes(2); //Get file structure type bytes
+				byte[] bBinaryVersion = breader.ReadBytes(2); //Get binary version
+				string sbHead = Encoding.ASCII.GetString(bHead);
+				string sbFileStructureType = Encoding.ASCII.GetString(bFileStructureType);
+				string sbBinaryVersion = Encoding.ASCII.GetString(bBinaryVersion);
 
+				if (sbHead == "JSB>")
+				{
+					if (sbFileStructureType == "BN")
+					{
+						//Check which binary version this file is
+						switch (sbBinaryVersion)
+						{
+							case "01":
+								MessageBox.Show("This is a JSB binary version 1 file");
+								DisplayDataChunkList(1);
+								break;
+							case "02":
+								MessageBox.Show("ERROR! Version 2 binary file support has not yet been implemented.");
+								return;
+						}
+					}
+					else
+					{
+						MessageBox.Show("Invalid file type! Got: " + sbFileStructureType + " Expected: BN");
+						return;
+					}
+				}
+				else
+				{
+					MessageBox.Show("Invalid header! Got: " + sbHead + " Expected: JSB>");
+					return;
+				}
+
+
+
+				MessageBox.Show(Encoding.ASCII.GetString(bHead));
 			}
-
-
-
-
-				DisplayDataChunkList(01);
 		}
-		
+
 		private void DisplayDataChunkList(int bVersion)
 		{
+			//Work out which type of binary to display data for
+			switch (bVersion)
+			{
+				case 1:
+					ClearEverything();
+					txtBinaryVersion.Text = "Version 1 - File Storage (BN01)";
+					//Need to find out how many 'files' are contained in this v1 binary
+					int dCount = GetDataChunkCount(1);
+					txtDataChunkCount.Text = "Files (" + dCount + ")";
+					break;
+			}
+		}
 
+		private void ClearEverything()
+		{
+			//Clear all text boxes and the data view
+			txtBinaryVersion.Clear();
+			txtDataChunkCount.Clear();
+		}
+
+		private static int GetDataChunkCount(int dType)
+		{
+			//put binary reader here
+
+			return dType; //TODO: change dtype to some new integer
 		}
 
 		private void btnOpen_Click(object sender, EventArgs e)
 		{
 			OpenBinaryFile();
+		}
+
+		private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+		{
+
 		}
 	}
 }
